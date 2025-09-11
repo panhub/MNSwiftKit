e3f6078796c5ebf96d3f266199d84f62c4cb80ce
mellow <mellow@mac.com>
2025-09-11 21:51:17 +0800

    [Update] 表情对UILabel的支持

--- MNSwiftKit/EmoticonKeyboard/Sources/MNEmoticonInputSupported.swift
+++ MNSwiftKit/EmoticonKeyboard/Sources/MNEmoticonInputSupported.swift
@@ -12,8 +12,26 @@ extension NameSpaceWrapper where Base: UITextView {
     
     /// 对attributedText解析表情后的内容
     public var plainText: String {
-        guard let attributedText = base.attributedText else { return "" }
-        return attributedText.mn.plainString
+        get {
+            guard let attributedText = base.attributedText else { return "" }
+            return attributedText.mn.plainString
+        }
+        set {
+            var font = base.font
+            if let attributedText = base.attributedText {
+                font = attributedText.mn.font
+            }
+            let attributedText = NSMutableAttributedString(string: newValue)
+            if let font = font {
+                attributedText.addAttribute(.font, value: font, range: .init(location: 0, length: attributedText.length))
+            }
+            attributedText.mn.matchsEmoticon()
+            let selectedRange = base.selectedRange
+            base.attributedText = attributedText
+            if selectedRange.location != NSNotFound {
+                base.selectedRange = NSRange(location: attributedText.length, length: 0)
+            }
+        }
     }
     
     /// 输入表情
@@ -52,5 +70,58 @@ extension NameSpaceWrapper where Base: UITextView {
         if selectedRange.location != NSNotFound {
             base.selectedRange = NSRange(location: selectedRange.location + attributedString.length, length: 0)
         }
+    }
+}
+
+extension NameSpaceWrapper where Base: UILabel {
+    
+    /// 对attributedText解析表情后的内容
+    public var plainText: String {
+        get {
+            guard let attributedText = base.attributedText else { return "" }
+            return attributedText.mn.plainString
+        }
+        set {
+            var font = base.font
+            if let attributedText = base.attributedText {
+                font = attributedText.mn.font
+            }
+            let attributedText = NSMutableAttributedString(string: newValue)
+            if let font = font {
+                attributedText.addAttribute(.font, value: font, range: .init(location: 0, length: attributedText.length))
+            }
+            attributedText.mn.matchsEmoticon()
+            base.attributedText = attributedText
+        }
+    }
+    
+    /// 追加表情
+    /// - Parameters:
+    ///   - emoticon: 表情图片
+    ///   - desc: 表情描述
+    public func append(_ emoticon: UIImage!, desc: String) {
+        guard desc.isEmpty == false else { return }
+        // 匹配表情附件
+        var attachment: MNEmoticonAttachment!
+        if let image = emoticon {
+            attachment = MNEmoticonAttachment(image: image, desc: desc)
+        } else {
+            attachment = MNEmoticonManager.shared.matchsEmoticon(in: desc).first
+        }
+        // 制作富文本
+        var attributedString: NSMutableAttributedString!
+        if let attachment = attachment {
+            attributedString = NSMutableAttributedString(attachment: attachment)
+            attributedString.addAttribute(.emoticonBacked, value: MNEmoticonBackedString(string: desc), range: NSMakeRange(0, attributedString.length))
+        } else {
+            attributedString = NSMutableAttributedString(string: desc)
+        }
+        let content = base.attributedText ?? NSAttributedString(string: "")
+        let font = content.mn.font ?? (base.font ?? .systemFont(ofSize: 17.0, weight: .regular))
+        attributedString.addAttribute(.font, value: font, range: NSMakeRange(0, attributedString.length))
+        // 拼接富文本
+        let attributedText = NSMutableAttributedString(attributedString: content)
+        attributedText.append(attributedString)
+        base.attributedText = attributedText
     }
 }
