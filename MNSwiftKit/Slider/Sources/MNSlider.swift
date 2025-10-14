5d48b9d097d5052d43339b6389cdfb39bf1e39e2
mellow <mellow@mac.com>
2025-10-14 18:01:42 +0800

    Slider更新<未完成>

--- MNSwiftKit/Slider/Sources/MNSlider.swift
+++ MNSwiftKit/Slider/Sources/MNSlider.swift
@@ -41,59 +41,76 @@ public class MNSlider: UIView {
     public private(set) var value: Double = 0.0
     /// 是否在拖拽
     public private(set) var isDragging: Bool = false
-    /// 内容视图
-    private lazy var contentView: UIView = {
-        let contentView = UIView(frame: bounds)
-        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        return contentView
-    }()
     /// 轨迹
-    private lazy var trackView: UIView = {
-        let trackView = UIView(frame: CGRect(x: 0.0, y: (contentView.frame.height - 4.0)/2.0, width: contentView.frame.width, height: 4.0))
-        trackView.clipsToBounds = true
-        trackView.backgroundColor = .lightGray
-        trackView.layer.cornerRadius = trackView.frame.height/2.0
-        trackView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin, .flexibleBottomMargin]
-        return trackView
-    }()
-    /// 进度
-    private lazy var progressView: UIView = {
-        let progressView = UIView(frame: trackView.bounds)
-        progressView.isUserInteractionEnabled = false
-        progressView.autoresizingMask = .flexibleHeight
-        progressView.backgroundColor = .white
-        return progressView
-    }()
+    private let trackView = UIView()
+    /// 进度条
+    private let progressView = UIView()
     /// 滑块
-    private lazy var thumbView: UIView = {
-        let thumbView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: contentView.frame.height, height: contentView.frame.height))
-        thumbView.contentMode = .scaleAspectFit
-        thumbView.backgroundColor = .white
-        thumbView.autoresizingMask = [.flexibleTopMargin, .flexibleBottomMargin]
-        thumbView.layer.cornerRadius = thumbView.frame.width/2.0
-        thumbView.layer.shadowRadius = 1.0
-        thumbView.layer.shadowOpacity = 0.25
-        thumbView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        thumbView.layer.shadowColor = UIColor.black.withAlphaComponent(0.3).cgColor
-        return thumbView
-    }()
-    /// 滑块图片
-    private lazy var thumbImageView: UIImageView = {
-        let thumbImageView = UIImageView(frame: thumbView.bounds)
-        thumbImageView.clipsToBounds = true
-        thumbImageView.contentMode = .scaleAspectFit
-        thumbImageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        return thumbImageView
-    }()
+    private let thumbView = UIView()
+    /// 滑块圆点
+    private let thumbImageView = UIImageView()
+    /// 轨迹高度约束
+    private var trackHeightConstraint: NSLayoutConstraint!
+    /// 滑块左侧约束
+    private var thumbLeftConstraint: NSLayoutConstraint!
+    /// 滑块宽度约束
+    private var thumbWidthConstraint: NSLayoutConstraint!
+    /// 进度条约束
+    private var progressLeftConstraint: NSLayoutConstraint!
     
     public override init(frame: CGRect) {
-        super.init(frame: CGRect(x: frame.minX, y: frame.minY, width: max(frame.width, 10.0), height: max(frame.height, 4.0)))
+        super.init(frame: frame)
         
-        addSubview(contentView)
-        contentView.addSubview(trackView)
+        trackView.clipsToBounds = true
+        trackView.backgroundColor = .lightGray
+        trackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(trackView)
+        trackHeightConstraint = trackView.heightAnchor.constraint(equalToConstant: 4.0)
+        NSLayoutConstraint.activate([
+            trackView.leftAnchor.constraint(equalTo: leftAnchor),
+            trackView.rightAnchor.constraint(equalTo: rightAnchor),
+            trackHeightConstraint,
+            trackView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        progressView.backgroundColor = .white
+        progressView.isUserInteractionEnabled = false
+        progressView.translatesAutoresizingMaskIntoConstraints = false
         trackView.addSubview(progressView)
-        contentView.addSubview(thumbView)
+        progressLeftConstraint = progressView.leftAnchor.constraint(equalTo: trackView.leftAnchor)
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: trackView.topAnchor),
+            progressLeftConstraint,
+            progressView.bottomAnchor.constraint(equalTo: trackView.bottomAnchor),
+            progressView.widthAnchor.constraint(equalToConstant: 0.0)
+        ])
+        
+        thumbView.backgroundColor = .white
+//        thumbView.layer.shadowRadius = 1.0
+//        thumbView.layer.shadowOpacity = 0.25
+//        thumbView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+//        thumbView.layer.shadowColor = UIColor.black.withAlphaComponent(0.3).cgColor
+        thumbView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(thumbView)
+        thumbLeftConstraint = thumbView.leftAnchor.constraint(equalTo: leftAnchor)
+        thumbWidthConstraint = thumbView.widthAnchor.constraint(equalToConstant: 15.0)
+        NSLayoutConstraint.activate([
+            thumbLeftConstraint,
+            thumbWidthConstraint,
+            thumbView.heightAnchor.constraint(equalToConstant: 15.0),
+            thumbView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        thumbImageView.clipsToBounds = true
+        thumbImageView.contentMode = .scaleAspectFit
+        thumbImageView.translatesAutoresizingMaskIntoConstraints = false
         thumbView.addSubview(thumbImageView)
+        NSLayoutConstraint.activate([
+            thumbImageView.topAnchor.constraint(equalTo: thumbView.topAnchor),
+            thumbImageView.leftAnchor.constraint(equalTo: thumbView.leftAnchor),
+            thumbImageView.bottomAnchor.constraint(equalTo: thumbView.bottomAnchor),
+            thumbImageView.rightAnchor.constraint(equalTo: thumbView.rightAnchor)
+        ])
         
         let pan = UIPanGestureRecognizer(target: self, action: #selector(pan(recognizer:)))
         pan.delegate = self
@@ -112,7 +129,24 @@ public class MNSlider: UIView {
     
     public override func layoutSubviews() {
         super.layoutSubviews()
-        updateSubviews()
+        setNeedsUpdateConstraints()
+        updateConstraintsIfNeeded()
+        trackView.layer.cornerRadius = trackHeightConstraint.constant/2.0
+    }
+    
+    public override func updateConstraintsIfNeeded() {
+        super.updateConstraintsIfNeeded()
+        let width = frame.width - thumbWidthConstraint.constant
+        thumbLeftConstraint.constant = width*value
+        progressLeftConstraint.constant = thumbLeftConstraint.constant + thumbWidthConstraint.constant/2.0
+    }
+    
+    /// 更新进度
+    private func updateProgress() {
+        let width = frame.width - thumbWidthConstraint.constant
+        guard width > 0.0 else { return }
+        let progress = thumbLeftConstraint.constant/width
+        value = Swift.max(0.0, Swift.min(1.0, progress))
     }
 }
 
@@ -120,10 +154,12 @@ public class MNSlider: UIView {
 extension MNSlider: UIGestureRecognizerDelegate {
     
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        if gestureRecognizer is UITapGestureRecognizer {
-            if let allow = delegate?.sliderShouldBeginTouching?(self), allow == false { return false}
-        } else if gestureRecognizer is UIPanGestureRecognizer {
-            if let allow = delegate?.sliderShouldBeginDragging?(self), allow == false { return false}
+        if let delegate = delegate {
+            if gestureRecognizer is UITapGestureRecognizer {
+                if let isAllow = delegate.sliderShouldBeginTouching?(self), isAllow == false { return false }
+            } else if gestureRecognizer is UIPanGestureRecognizer {
+                if let isAllow = delegate.sliderShouldBeginDragging?(self), isAllow == false { return false }
+            }
         }
         return true
     }
@@ -135,70 +171,59 @@ private extension MNSlider {
     @objc func pan(recognizer: UIPanGestureRecognizer) {
         switch recognizer.state {
         case .began:
-            delegate?.sliderWillBeginDragging?(self)
+            if let delegate = delegate {
+                delegate.sliderWillBeginDragging?(self)
+            }
             isDragging = true
         case .changed:
             let translation = recognizer.translation(in: recognizer.view)
             recognizer.setTranslation(.zero, in: recognizer.view)
-            var frame = thumbView.frame
-            frame.origin.x += translation.x
-            frame.origin.x = min(max(0.0, frame.minX), contentView.frame.width - frame.width)
-            thumbView.frame = frame
-            frame = progressView.frame
-            frame.size.width = thumbView.frame.midX
-            progressView.frame = frame
+            var constant = thumbLeftConstraint.constant
+            constant += translation.x
+            thumbLeftConstraint.constant = Swift.max(0.0, Swift.min(constant, frame.width - thumbWidthConstraint.constant))
+            progressLeftConstraint.constant = thumbLeftConstraint.constant + thumbWidthConstraint.constant/2.0
             updateProgress()
-            delegate?.sliderDidDragging?(self)
+            if let delegate = delegate {
+                delegate.sliderDidDragging?(self)
+            }
         case .ended:
             isDragging = false
-            delegate?.sliderDidEndDragging?(self)
+            if let delegate = delegate {
+                delegate.sliderDidEndDragging?(self)
+            }
         default:
             isDragging = false
         }
     }
     
     @objc func tap(recognizer: UITapGestureRecognizer) {
-        delegate?.sliderWillBeginTouching?(self)
-        let location = recognizer.location(in: contentView)
-        let width = contentView.frame.width - thumbView.frame.width
-        value = min(max(0.0, location.x - thumbView.frame.width/2.0), width)/width
-        updateSubviews()
-        delegate?.sliderDidEndTouching?(self)
-    }
-    
-    private func updateProgress() {
-        let width = contentView.frame.width - thumbView.frame.width
-        value = min(max(0.0, thumbView.frame.midX - thumbView.frame.width/2.0), width)/width
-    }
-    
-    private func updateSubviews() {
-        var frame = thumbView.frame
-        frame.origin.x = (contentView.frame.width - frame.width)*value
-        frame.origin.x = min(max(0.0, frame.minX), contentView.frame.width - frame.width)
-        thumbView.frame = frame
-        frame = progressView.frame
-        frame.size.width = thumbView.frame.midX
-        progressView.frame = frame
+        if let delegate = delegate {
+            delegate.sliderWillBeginTouching?(self)
+        }
+        let location = recognizer.location(in: self)
+        thumbLeftConstraint.constant = Swift.max(0.0, Swift.min(location.x, frame.width - thumbWidthConstraint.constant))
+        progressLeftConstraint.constant = thumbLeftConstraint.constant + thumbWidthConstraint.constant/2.0
+        updateProgress()
+        if let delegate = delegate {
+            delegate.sliderDidEndTouching?(self)
+        }
     }
 }
 
 // MARK: - 修改进度
 extension MNSlider {
     
-    public func setValue(_ value: Float, animated: Bool = false) {
+    public func setValue<T>(_ value: T, animated: Bool = false) where T: BinaryFloatingPoint {
         setValue(Double(value), animated: animated)
     }
     
-    public func setValue(_ value: CGFloat, animated: Bool = false) {
-        setValue(Double(value), animated: animated)
-    }
-    
-    public func setValue(_ value: Double, animated: Bool = false) {
+    public func updateProgress(_ progress: Double, animated: Bool = false) {
         guard isDragging == false else { return }
-        self.value = min(max(0.0, value), 1.0)
+        value = Swift.max(0.0, Swift.min(progress, 1.0))
         let animations: ()->Void = { [weak self] in
             guard let self = self else { return }
-            self.updateSubviews()
+            self.setNeedsLayout()
+            self.layoutIfNeeded()
         }
         if animated {
             UIView.animate(withDuration: 0.25, delay: 0.0, options: .curveEaseInOut, animations: animations)
@@ -213,18 +238,8 @@ extension MNSlider {
     
     // 修改轨迹
     public var trackHeight: CGFloat {
-        get { trackView.frame.height }
-        set {
-            if trackView.frame.height == newValue { return }
-            let autoresizingMask = trackView.autoresizingMask
-            trackView.autoresizingMask = []
-            var frame = trackView.frame
-            frame.size.height = newValue
-            frame.origin.y = (contentView.frame.height - newValue)/2.0
-            trackView.frame = frame
-            trackView.autoresizingMask = autoresizingMask
-            trackView.layer.cornerRadius = frame.height/2.0
-        }
+        get { trackHeightConstraint.constant }
+        set { trackHeightConstraint.constant = newValue }
     }
     
     /// 轨迹颜色
