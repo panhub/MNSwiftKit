04ca20d7fe9d73e87a8d22eabefea8493b9a1ce4
pan <mini@mac.com>
2025-12-10 02:00:57 +0800

    [Update] SPM配置

--- MNSwiftKit/Full/Full.swift
+++
@@ -1,6 +0,0 @@
-//
-//  Full.swift
-//  MNSwiftKit
-//
-//  Created by panhub on 2025/8/25.
-//  对于SMP占位
---
+++ MNSwiftKit/Full/MNSwiftKit.swift
@@ -0,0 +1,34 @@
+//
+//  MNSwiftKit.swift
+//  MNSwiftKit
+//
+//  Created by panhub on 2025/8/25.
+//  对于SMP占位
+
+#if SWIFT_PACKAGE
+@_exported import Base
+@_exported import Toast
+@_exported import Slider
+@_exported import Utility
+@_exported import Player
+@_exported import Layout
+@_exported import Refresh
+@_exported import Request
+@_exported import Purchase
+@_exported import Database
+@_exported import Definition
+@_exported import Extension
+@_exported import EmptyView
+@_exported import Networking
+@_exported import AssetPicker
+@_exported import NameSpace
+@_exported import PageControl
+@_exported import Components
+@_exported import MediaExport
+@_exported import Transitioning
+@_exported import AssetBrowser
+@_exported import SplitController
+@_exported import AnimatedImage
+@_exported import CollectionLayout
+@_exported import EmoticonKeyboard
+#endif
